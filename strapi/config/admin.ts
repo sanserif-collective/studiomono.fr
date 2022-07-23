fc79b4f67580e77bef220bfea8279aa0fb30d61c
Benjamin Dos Santos <hello@benjamindossantos.be>
2022-07-23 10:06:14 +0200

    feat(strapi): add api token salt

--- strapi/config/admin.ts
+++ strapi/config/admin.ts
@@ -1,5 +1,6 @@
 export default ({ env }) => ({
   auth: {
     secret: env('ADMIN_JWT_SECRET', '2a77ac5735e05b87af24b76b31f16bff'),
+    apiToken: env('API_TOKEN_SALT', '6TnmdVxWrk/SgarZ1MtTCg==')
   },
 });
