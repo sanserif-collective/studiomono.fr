dbce538600474689a94904c510f0b3cfc770c150
Benjamin Dos Santos <hello@benjamindossantos.be>
2022-07-27 19:49:59 +0200

    feat: update packages

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -1,8 +1,7 @@
-import image from '@astrojs/image'
 import tailwind from '@astrojs/tailwind'
 import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), image()]
-});
+  integrations: [tailwind()]
+})
