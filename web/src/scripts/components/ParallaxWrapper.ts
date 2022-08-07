ab2deb17f177b0fb14452b3317f977228f7f053a
Benjamin Dos Santos <hello@benjamindossantos.be>
2022-08-07 21:11:08 +0200

    feat(web): disable parallax component

--- web/src/scripts/components/ParallaxWrapper.ts
+++ web/src/scripts/components/ParallaxWrapper.ts
@@ -3,10 +3,32 @@ import { gsap } from 'gsap/all'
 export default class ParallaxWrapper extends HTMLElement {
   private parallax: gsap.core.Tween
 
-  public connectedCallback() {
+  private init(matches: boolean) {
+    this.parallax?.kill()
+
+    if (matches) {
+      // gsap.set(this, { clearProps: true })
+
+      return this.parallax = gsap.from(this, {
+        yPercent: this.offsetWidth > this.offsetHeight ? 50 : 100,
+        duration: 5,
+        paused: true,
+        scrollTrigger: {
+          horizontal: false,
+          trigger: this,
+          scrub: 0.1,
+          start: 'top bottom',
+          end: 'bottom top'
+        }
+      })
+    }
+
+    // gsap.set(this, { clearProps: true })
+
     this.parallax = gsap.from(this, {
       xPercent: this.offsetWidth > this.offsetHeight ? 50 : 100,
       duration: 5,
+      paused: true,
       scrollTrigger: {
         trigger: this,
         scrub: 0.1,
@@ -16,7 +38,14 @@ export default class ParallaxWrapper extends HTMLElement {
     })
   }
 
+  constructor() {
+    super()
+
+    // app.plugins.portrait.events.add(({ matches }) => this.init(matches))
+    // this.init(app.plugins.portrait.media.matches)
+  }
+
   public disconnectedCallback() {
-    this.parallax.kill()
+    this.parallax?.kill()
   }
 }
