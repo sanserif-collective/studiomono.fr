c59ef47b11137a2df693f0bffa6dfaac8e1ed2b1
Benjamin Dos Santos <hello@benjamindossantos.be>
2022-08-10 22:49:43 +0200

    feat(strapi): change admin url

--- strapi/config/admin.ts
+++ strapi/config/admin.ts
@@ -3,4 +3,5 @@ export default ({ env }) => ({
     secret: env('ADMIN_JWT_SECRET', '2a77ac5735e05b87af24b76b31f16bff'),
     apiToken: env('API_TOKEN_SALT', '6TnmdVxWrk/SgarZ1MtTCg==')
   },
+  url: '/dashboard'
 });
