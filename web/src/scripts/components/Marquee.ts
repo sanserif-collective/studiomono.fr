87fb5b17c4fd7d9f3ec4da427900919f30db5fb4
Benjamin Dos Santos <hello@benjamindossantos.be>
2022-08-14 02:53:38 +0200

    feat(web): add project card hover

--- web/src/scripts/components/Marquee.ts
+++ web/src/scripts/components/Marquee.ts
@@ -1,9 +1,15 @@
 import { gsap } from 'gsap/all'
 
 export class Marquee extends HTMLElement {
+  private delayedPlay: gsap.core.Tween
+  private delayedPause: gsap.core.Tween
+
   private playing = this.getAttribute('playing') ?? 'lazy'
-  private direction = this.getAttribute('direction')
+  private direction = this.getAttribute('direction') ?? '100'
   private pauseOnHover = this.hasAttribute('pauseonhover')
+  private playOnHover = this.hasAttribute('playonhover')
+  private delayBeforePlay = Number(this.getAttribute('delaybeforeplay') ?? '0')
+  private delayBeforePause = Number(this.getAttribute('delaybeforepause') ?? '0')
 
   private slide = gsap
     .to(this.children, {
@@ -14,11 +20,43 @@ export class Marquee extends HTMLElement {
       paused: this.playing === 'lazy'
     })
 
-  private onMouseEnter = () => this.pauseOnHover && this.slide.pause()
-  private onMouseLeave = () => this.pauseOnHover && this.slide.play()
+  private onMouseEnter = () => {
+    this.pauseOnHover && this.pause()
+    this.playOnHover && this.play()
+  }
 
-  public play = () => this.slide.play()
-  public pause = () => this.slide.pause()
+  private onMouseLeave = () => {
+    this.pauseOnHover && this.play()
+    this.playOnHover && this.pause()
+  }
+
+  public play = () => {
+    this.delayedPause?.kill()
+
+    if (this.delayBeforePlay) {
+      return this.delayedPlay = gsap
+        .delayedCall(
+          this.delayBeforePlay,
+          () => this.slide.play()
+        )
+    }
+
+    this.slide.play()
+  }
+
+  public pause = () => {
+    this.delayedPlay?.kill()
+
+    if (this.delayBeforePause) {
+      return this.delayedPause = gsap
+        .delayedCall(
+          this.delayBeforePause,
+          () => this.slide.pause()
+        )
+    }
+
+    this.slide.pause()
+  }
 
   public connectedCallback() {
     this.addEventListener('mouseenter', this.onMouseEnter)
