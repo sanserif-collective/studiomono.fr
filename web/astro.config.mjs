b97e9fc96468e5351714cfbbdc2bf3ef6470e819
Benjamin Dos Santos <hello@benjamindossantos.be>
2022-09-04 22:55:37 +0200

    feat(web): add sitemap.xml

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -1,8 +1,10 @@
-import tailwind from '@astrojs/tailwind'
-import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind';
+import { defineConfig } from 'astro/config';
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), sitemap()],
   site: 'https://sanserif.be'
-})
+});
