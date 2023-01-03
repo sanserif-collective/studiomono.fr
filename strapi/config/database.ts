782c6dd54259224366193c5c06ad3b8b23c1585f
Benjamin Dos Santos <hello@benjamindossantos.be>
2023-01-03 21:25:47 +0100

    feat(strapi): add db pooling

--- strapi/config/database.ts
+++ strapi/config/database.ts
@@ -10,4 +10,14 @@ export default ({ env }) => ({
       ssl: env.bool('DATABASE_SSL', false),
     },
   },
+  acquireConnectionTimeout: 5000,
+  pool: {
+    min: 0,
+    max: 10,
+    createTimeoutMillis: 8000,
+    acquireTimeoutMillis: 8000,
+    idleTimeoutMillis: 8000,
+    reapIntervalMillis: 1000,
+    createRetryIntervalMillis: 100,
+  },
 });
