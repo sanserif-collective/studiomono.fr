e8ac69011067eb3497dbe97f3d165e569e33f036
Benjamin Dos Santos <hello@benjamindossantos.be>
2024-03-04 19:53:45 +0100

    feat: migrate to cloudflare

--- web/astro.config.ts
+++ web/astro.config.ts
@@ -1,13 +1,11 @@
 import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
-import vercel from '@astrojs/vercel/serverless';
+import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
   integrations: [tailwind(), sitemap()],
   site: 'https://sanserif.be',
   output: 'server',
-  adapter: vercel({
-    functionPerRoute: true,
-  }),
+  adapter: cloudflare(),
 });
