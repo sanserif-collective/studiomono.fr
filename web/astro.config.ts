18c49d15d484d8db81a60880cd2d7e5086604a98
Benjamin Dos Santos <hello@benjamindossantos.be>
2024-03-04 17:01:13 +0100

    feat: static generation to ssr

--- web/astro.config.ts
+++ web/astro.config.ts
@@ -1,8 +1,13 @@
 import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
+import vercel from '@astrojs/vercel/serverless';
 
 export default defineConfig({
   integrations: [tailwind(), sitemap()],
   site: 'https://sanserif.be',
+  output: 'server',
+  adapter: vercel({
+    functionPerRoute: true,
+  }),
 });
