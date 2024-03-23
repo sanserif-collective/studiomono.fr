443a8a6b39fdcef62e326b92dc4dd7d1742bf211
Benjamin Dos Santos <hello@benjamindossantos.be>
2024-03-23 13:41:42 +0100

    feat: inject BASE_URL and PLAUSIBLE_URL

--- web/astro.config.ts
+++ web/astro.config.ts
@@ -2,10 +2,13 @@ import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import cloudflare from '@astrojs/cloudflare';
+import { loadEnv } from 'vite';
+
+const { BASE_URL } = loadEnv(process.env.NODE_ENV!, process.cwd(), '');
 
 export default defineConfig({
   integrations: [tailwind(), sitemap()],
-  site: 'https://sanserif.be',
+  site: BASE_URL,
   output: 'server',
   adapter: cloudflare(),
 });
--- web/src/env.ts
+++ web/src/env.ts
@@ -3,3 +3,6 @@ import e from 'env-var';
 const env = e.from(typeof import.meta === 'undefined' ? process.env : import.meta.env);
 
 export const STRAPI_URL = env.get('STRAPI_URL').required().asUrlString();
+export const BASE_URL = env.get('BASE_URL').required().asUrlString();
+export const PLAUSIBLE_DOMAIN = env.get('PLAUSIBLE_DOMAIN').asString();
+export const PLAUSIBLE_URL = env.get('PLAUSIBLE_URL').default(BASE_URL).asUrlString();
