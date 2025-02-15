60de20a3075d8a32f625ce3bc460ac59cc4229c3
Benjamin Dos Santos <hello@benjamindossantos.be>
2025-02-15 15:31:25 +0000

    chore: update dependencies

--- web/astro.config.ts
+++ web/astro.config.ts
@@ -5,35 +5,32 @@ import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
   integrations: [tailwind(), sitemap()],
-  site: 'https://sanserif.be',
   output: 'server',
   adapter: cloudflare({
     platformProxy: {
       enabled: false,
     },
   }),
-  experimental: {
-    env: {
-      schema: {
-        BASE_URL: envField.string({
-          context: 'server',
-          access: 'public',
-        }),
-        STRAPI_URL: envField.string({
-          context: 'server',
-          access: 'secret',
-        }),
-        PLAUSIBLE_DOMAIN: envField.string({
-          context: 'server',
-          access: 'public',
-          optional: true,
-        }),
-        PLAUSIBLE_URL: envField.string({
-          context: 'server',
-          access: 'public',
-          optional: true,
-        }),
-      },
+  env: {
+    schema: {
+      BASE_URL: envField.string({
+        context: 'server',
+        access: 'public',
+      }),
+      STRAPI_URL: envField.string({
+        context: 'server',
+        access: 'secret',
+      }),
+      PLAUSIBLE_DOMAIN: envField.string({
+        context: 'server',
+        access: 'public',
+        optional: true,
+      }),
+      PLAUSIBLE_URL: envField.string({
+        context: 'server',
+        access: 'public',
+        optional: true,
+      }),
     },
   },
   server: {
--- web/src/strapi.ts
+++ web/src/strapi.ts
@@ -1,4 +1,4 @@
 import { STRAPI_URL } from 'astro:env/server';
 import { GraphQLClient } from 'graphql-request';
 
-export const strapi = new GraphQLClient(STRAPI_URL, { fetch: globalThis.fetch });
+export const strapi = new GraphQLClient(STRAPI_URL, { fetch });
